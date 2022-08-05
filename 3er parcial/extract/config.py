af379858cc2eb4f7037bfeeccc84d01d7387f33b
Anahi Rocha <anhai25rrz@gmail.com>
2022-08-05 00:39:35 -0500

    Se corrige etapa Extract

--- 3er parcial/extract/config.py
+++ 3er parcial/extract/config.py
@@ -3,13 +3,17 @@ from sqlalchemy import create_engine
 
 #Definimos una variable global para la configuracion
 
-#Verificamos si existe la configuración y si no la cargamos
-def connection():
+def mysqlconnect(): 
+    #Creamos conexion a la base de datos de manera local
     try:
-        #Creamos conexion a la base de datos de manera local
-        conn='mysql+pymysql://root:root@localhost:3306/encuentacovid'
-        conexion = create_engine(conn)
+        conn = pymysql.connect( 
+            host='localhost', 
+            user='root',  
+            password = "root", 
+            db='encuentacovid', 
+            ) 
         print("Se hizo la conexion a la base de datos encuestacovid")
-        return conexion
-    except:
-        print("Ocurrió un problema al realizar la conexión a la Base de datos")
+        return conn
+    except:        
+        print("Ocurrió un problema al realizar la conexión a la Base de datos") 
+    
